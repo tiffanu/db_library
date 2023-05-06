a18a6e90d3dea0f5f113a472a326d17981c08c08
jukov artem <iukov.ai@phystech.edu>
2023-05-06 02:52:49 +0000

    Update test2.py

--- tests/test2.py
+++ tests/test2.py
@@ -28,15 +28,15 @@ def test2(select_df):
     assert select_df.query("name == 'Артём'")['begin_date'].iloc[0] == datetime.date(2023, 2, 5)
     assert select_df.query("name == 'Артём'")['end_date'].iloc[0] == datetime.date(2023, 2, 6)
     
-    assert select_df.query("name == 'Тимур'")['surname'].iloc[0] == 'Муцураев'
-    assert select_df.query("name == 'Тимур'")['begin_date'].iloc[0] == datetime.date(1994, 12, 11)
-    assert select_df.query("name == 'Тимур'")['end_date'].iloc[0] == datetime.date(1996, 8, 31)
+    assert select_df.query("name == 'Альбина'")['surname'].iloc[0] == 'Рухадзе'
+    assert select_df.query("name == 'Альбина'")['begin_date'].iloc[0] == datetime.date(2023, 3, 5)
+    assert select_df.query("name == 'Альбина'")['end_date'].iloc[0] == datetime.date(2023, 3, 6)
 
-    assert select_df.query("name == 'Тимур'")['surname'].iloc[0] == 'Муцураев'
-    assert select_df.query("name == 'Тимур'")['begin_date'].iloc[0] == datetime.date(1994, 12, 11)
-    assert select_df.query("name == 'Тимур'")['end_date'].iloc[0] == datetime.date(1996, 8, 31)
+    assert select_df.query("name == 'Артём'")['surname'].iloc[0] == None
+    assert select_df.query("name == 'Артём'")['begin_date'].iloc[0] == datetime.date(2023, 7, 23)
+    assert select_df.query("name == 'Артём'")['end_date'].iloc[0] == datetime.date(2023, 8, 23)
     
-    assert select_df.query("name == 'Тимур'")['surname'].iloc[0] == 'Муцураев'
-    assert select_df.query("name == 'Тимур'")['begin_date'].iloc[0] == datetime.date(1994, 12, 11)
-    assert select_df.query("name == 'Тимур'")['end_date'].iloc[0] == datetime.date(1996, 8, 31)
+    assert select_df.query("name == 'Александр'")['surname'].iloc[0] == 'Левин'
+    assert select_df.query("name == 'Александр'")['begin_date'].iloc[0] == datetime.date(2023, 10, 6)
+    assert select_df.query("name == 'Александр'")['end_date'].iloc[0] == datetime.date(2023, 10, 8)
     
