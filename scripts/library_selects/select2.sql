b8ed90be4d8d67390f86bdff5d2b3af3a22ec519
jukov artem <iukov.ai@phystech.edu>
2023-05-09 08:30:21 +0000

    Revert "Merge branch 'dev' into 'main'"

--- scripts/library_selects/select2.sql
+++
@@ -1,5 +0,0 @@
--- Вывести всех посетителей, взявших книги, 
--- и отсортировать по дате начала брони
-select name, surname, begin_date, end_date
-from library.Reserve
-order by begin_date, end_date;
--- scripts/library_selects/select5.sql
+++
@@ -1,6 +0,0 @@
--- Вывести посетителей и кол-во книг, которые они брали.
--- Отсортировать по этому количеству
-select distinct name, surname, count(book_id) over (partition by (name, surname)) as reserved_books
-from library.Book_Reserve as br
-join library.Reserve as r on r.res_id = br.res_id
-order by reserved_books desc;
