3e369b7dc3ddaae5ea630ee23e19f30e97ba5977
jukov artem <iukov.ai@phystech.edu>
2023-05-09 05:52:31 +0300

    edit test3

--- tests/test_view3.py
+++ tests/test_view3.py
@@ -22,16 +22,16 @@ def select_df(select_script, sqlalchemy_conn):
 def test3(select_df):
     assert select_df.query("res_id == 4")['name'].iloc[0] == 'Александр'
     assert select_df.query("res_id == 4")['surname'].iloc[0] == 'Левин'
-    assert select_df.query("res_id == 4")['phone'].iloc[0] == '+7-963-***-**-**'
+    assert select_df.query("res_id == 4")['phone'].iloc[0] == None
 
     assert select_df.query("res_id == 2")['name'].iloc[0] == 'Альбина'
     assert select_df.query("res_id == 2")['surname'].iloc[0] == 'Рухадзе'
-    assert select_df.query("res_id == 2")['phone'].iloc[0] == '+7-963-***-**-**'
+    assert select_df.query("res_id == 2")['phone'].iloc[0] == None
 
     assert select_df.query("res_id == 1")['name'].iloc[0] == 'Артём'
     assert select_df.query("res_id == 1")['surname'].iloc[0] == None
-    assert select_df.query("res_id == 1")['phone'].iloc[0] == '+7-965-***-**-**'
+    assert select_df.query("res_id == 1")['phone'].iloc[0] == None
 
     assert select_df.query("res_id == 3")['name'].iloc[0] == 'Тимур'
     assert select_df.query("res_id == 3")['surname'].iloc[0] == 'Муцураев'
-    assert select_df.query("res_id == 3")['phone'].iloc[0] == '+7-963-***-**-**'
+    assert select_df.query("res_id == 3")['phone'].iloc[0] == None
